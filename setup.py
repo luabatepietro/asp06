745c27314ef87596a86b8e23334ea3180fd2f54f
luabatepietro <luabatepietro@hotmail.com>
2024-10-22 16:09:21 -0300

    setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,16 @@ setup(
     version="0.1.0",  # Versão do pacote
     packages=find_packages(),  # Encontrar automaticamente todos os pacotes
     install_requires=[  # Dependências
+        'matplotlib',
         'numpy',
+        'pygame',
+        'opencv-python',
+        'scipy',
+        'scikit-learn',
+        'autograd',
+        'pandas',
+        'jupyter'
+
     ],
     entry_points={
         "console_scripts": [
